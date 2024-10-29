2a43ab114638bfda8a628e22d617fd23453543fa
Matteo <mbobasso@fi.uba.ar>
2024-10-29 10:25:28 -0300

    cambios index.js

--- index.js
+++ index.js
@@ -4,8 +4,24 @@ const port = 3000
 
 app.get('/', (req, res) => {
     const user_agent = req.get('user-agent')
-    res.header('Server', 'intro al Des. de Software')
-    res.send(`Hola! Tu user agent es: ${user_agent}`)
+    res.header('Server', 'Intro al Des. de Software')
+    res.header('Content-Type', 'text/html')
+    let body = ""
+    if (user_agent.includes("Linux")){
+        body = `<h1>El link de descarga para Linux es: ....</h1>`
+    } else if (user_agent.includes("Windows")){
+        body = `<h1>El link de descarga para Windows es: ...</h1>`
+    } else if (user_agent.includes("Mac Os")){
+        body = `<h1>El link de descarga para MacOS es: ...</h1>`
+    } else{
+        body = `No conozco tu software`
+    }
+    
+    res.send(body)
+})
+
+app.get('/download', (req, res)=> {
+    res.send('Iniciando descarga...')
 })
 
 app.listen(port, () => {
