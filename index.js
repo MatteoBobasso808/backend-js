63c650ae18008a46556f7dec0b365487c50b8fdb
Matteo <mbobasso@fi.uba.ar>
2024-10-31 08:48:12 -0300

    cambios

--- index.js
+++ index.js
@@ -2,26 +2,58 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    const user_agent = req.get('user-agent')
-    res.header('Server', 'Intro al Des. de Software')
-    res.header('Content-Type', 'text/html')
-    let body = ""
-    if (user_agent.includes("Linux")){
-        body = `<h1>El link de descarga para Linux es: ....</h1>`
-    } else if (user_agent.includes("Windows")){
-        body = `<h1>El link de descarga para Windows es: ...</h1>`
-    } else if (user_agent.includes("Mac Os")){
-        body = `<h1>El link de descarga para MacOS es: ...</h1>`
-    } else{
-        body = `No conozco tu software`
+let languages = [
+    {
+        id: 1,
+        name: "C",
+        creation_date: 1972,
+        last_version: "C17"
+    },
+    {
+        id: 2,
+        name: "Python",
+        creation_date: 1986,
+        last_version: "P19"
+    },
+    {
+        id: 3,
+        name: "HTML",
+        creation_date: 1990,
+        last_version: "H5"
+    },
+    {
+        id: 4,
+        name: "Javascript",
+        creation_date: 1975,
+        last_version: "J22"
+    },
+    {
+        id: 5,
+        name: "Java",
+        creation_date: 1990,
+        last_version: "16"
     }
-    
-    res.send(body)
+]
+
+app.get('/', (req, res) => {
+    res.send("")
 })
 
-app.get('/download', (req, res)=> {
-    res.send('Iniciando descarga...')
+// app.use(express.json()) --> acepta json como entrada
+
+app.get('/api/v1/languages', (req, res) => {
+    res.send(languages)
+})
+
+app.get('/api/v1/languages/:id', (req, res) => {
+    const id = req.params.id
+    const language = languages.find(element => element.id === parseInt(id))
+    if(language === undefined){
+        res.status(404).send("Language not found")
+        return
+    } else {
+        res.send(language)
+    }
 })
 
 app.listen(port, () => {
